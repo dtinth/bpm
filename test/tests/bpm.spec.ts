df96d57c55a856522d7fe45d3092fad1d4468d0b
dtinth on MBP M1 <dtinth@spacet.me>
2023-01-22 18:57:44 +0700

    Allow resetting bpm without refreshing page

--- test/tests/bpm.spec.ts
+++ test/tests/bpm.spec.ts
@@ -20,4 +20,22 @@ test('displays the bpm after tapping 5 times', async ({ page }) => {
   await expect(page.locator('body')).toContainText('120')
 })
 
+test('resets the bpm when beat is not kept', async ({ page }) => {
+  await page.goto('/')
+  await page.evaluate(() => tap(1111000))
+  await page.evaluate(() => tap(1111500))
+  await page.evaluate(() => tap(1112000))
+  await page.evaluate(() => tap(1112500))
+  await page.evaluate(() => tap(1113000))
+  await expect(page.locator('body')).toContainText('120')
+
+  await page.evaluate(() => tap(2111000))
+  await expect(page.locator('body')).toContainText('TAP')
+  await page.evaluate(() => tap(2112000))
+  await page.evaluate(() => tap(2113000))
+  await page.evaluate(() => tap(2114000))
+  await page.evaluate(() => tap(2115000))
+  await expect(page.locator('body')).toContainText('60')
+})
+
 declare var tap: (t: number) => void
